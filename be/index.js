754d53d09868bf0df56a46b15269bb0bb9520acc
Aqib <maqib82001@gmail.com>
2024-11-27 08:51:20 +0530

    added start command

--- be/index.js
+++ be/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const dbConnect = require("./config/dbConnect");
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
+const dbConnect = require("./src/config/dbConnect.js");
+const authRoutes = require("./src/routes/authRoutes.js");
+const userRoutes = require("./src/routes/userRoutes.js");
 dbConnect();
 
 const app = express();
